a673680d9d919a2db97b725477c0fea9235c3f95
Edgar Coime <e.coimeii@hotmail.com>
2024-11-14 10:55:06 -0800

    fix: add graceful shutdown to proxy

--- proxy.py
+++ proxy.py
@@ -1,13 +1,29 @@
 import ipaddress
-from socket import AF_INET, socket, SOCK_DGRAM
 import random
+import signal
+import sys
 import time
 from concurrent.futures import ThreadPoolExecutor
+from socket import AF_INET, SOCK_DGRAM, socket
+
 from utils.cli import CLI
 
 
-class Proxy():
-    def __init__(self, listen_ip, listen_port: int, target_ip: str, target_port: int, client_drop: int, client_delay: int, client_delay_time: float, server_drop: float, server_delay: float, server_delay_time: float):
+class Proxy:
+    def __init__(
+        self,
+        listen_ip,
+        listen_port: int,
+        target_ip: str,
+        target_port: int,
+        client_drop: int,
+        client_delay: int,
+        client_delay_time: float,
+        server_drop: float,
+        server_delay: float,
+        server_delay_time: float,
+    ):
+        self.running = True
         self.listen_ip = listen_ip
         self.listen_port = listen_port
         self.socket = socket(AF_INET, SOCK_DGRAM)
@@ -16,51 +32,71 @@ class Proxy():
             "port": None,
             "drop": client_drop,
             "delay": client_delay,
-            "delay_time": client_delay_time
+            "delay_time": client_delay_time,
         }
         self.server_config = {
             "ip": target_ip,
             "port": target_port,
             "drop": server_drop,
             "delay": server_delay,
-            "delay_time": server_delay_time
+            "delay_time": server_delay_time,
         }
 
-
     def set_config(self, target, field, value):
-        config = self.client_config if target == 'client' else self.server_config
+        config = self.client_config if target == "client" else self.server_config
         config[field] = value
 
-
     def get_config(self, target, field):
-        config = self.client_config if target == 'client' else self.server_config
+        config = self.client_config if target == "client" else self.server_config
         return config[field]
 
-
     def run(self):
+        self.running = True
         self.socket.bind((str(ipaddress.ip_address(self.listen_ip)), self.listen_port))
-        while True:
+        while self.running:
             data, (ip, port) = self.socket.recvfrom(65535)
-            is_server = ip == self.server_config["ip"] and port == self.server_config["port"]
+            is_server = (
+                ip == self.server_config["ip"] and port == self.server_config["port"]
+            )
             if not is_server:
-                self.client_config["ip"] = ip 
+                self.client_config["ip"] = ip
                 self.client_config["port"] = port
-            forward_to = (str(ipaddress.ip_address(self.client_config["ip"] if is_server else self.server_config["ip"])), self.client_config["port"] if is_server else self.server_config["port"])
+            forward_to = (
+                str(
+                    ipaddress.ip_address(
+                        self.client_config["ip"]
+                        if is_server
+                        else self.server_config["ip"]
+                    )
+                ),
+                self.client_config["port"] if is_server else self.server_config["port"],
+            )
             config = self.server_config if is_server else self.client_config
             drop_prob = config["drop"]
             delay_prob = config["delay"]
             delay_time = config["delay_time"]
 
-            shouldDrop = random.random() <= (drop_prob / 100)
-            shouldDelay = random.random() <= (delay_prob / 100)
-            if shouldDrop:
+            should_drop = random.random() <= (drop_prob / 100)
+            should_delay = random.random() <= (delay_prob / 100)
+            if should_drop:
                 continue
-            if shouldDelay:
+            if should_delay:
                 time.sleep(delay_time / 1000)
             self.socket.sendto(data, forward_to)
 
+    def stop(self):
+        self.running = False
+        self.socket.close()
 
-if __name__ == "__main__":
+
+def signal_handler(_sig, _frame, proxy: Proxy, cli: CLI):
+    print("Shutting down gracefully...")
+    proxy.stop()  # Stop the proxy loop
+    cli.stop()
+    sys.exit(0)
+
+
+def main():
     proxy = Proxy(
         listen_ip="0.0.0.0",
         listen_port=4000,
@@ -77,62 +113,67 @@ if __name__ == "__main__":
         [
             "Proxy Parameters",
             "UP/DOWN arrows to select value, LEFT/RIGHT to adjust",
-        ], 
+        ],
         [
             {
-                "name": "Client drop", 
-                "min": 0, 
-                "max": 100, 
-                "step": 10, 
+                "name": "Client drop",
+                "min": 0,
+                "max": 100,
+                "step": 10,
                 "suffix": "%",
-                "get_value": lambda: proxy.get_config("client","drop"), 
-                "set_value": lambda x: proxy.set_config("client", "drop", x)
+                "get_value": lambda: proxy.get_config("client", "drop"),
+                "set_value": lambda x: proxy.set_config("client", "drop", x),
             },
             {
                 "name": "Client delay",
-                "min": 0, 
-                "max": 100, 
-                "step": 10, 
-                "suffix": "%",
-                "get_value": lambda: proxy.get_config("client","delay"), 
-                "set_value": lambda x: proxy.set_config("client", "delay", x)
-            },
-            {
-                "name": "Client delay time", 
                 "min": 0,
-                "step": 100, 
-                "suffix": "ms",
-                "get_value": lambda: proxy.get_config("client","delay_time"), 
-                "set_value": lambda x: proxy.set_config("client", "delay_time", x)
-            },
-            {
-                "name": "Server drop", 
-                "min": 0, 
-                "max": 100, 
-                "step": 10, 
+                "max": 100,
+                "step": 10,
                 "suffix": "%",
-                "get_value": lambda: proxy.get_config("server","drop"), 
-                "set_value": lambda x: proxy.set_config("server", "drop", x)
+                "get_value": lambda: proxy.get_config("client", "delay"),
+                "set_value": lambda x: proxy.set_config("client", "delay", x),
             },
             {
-                "name": "Server delay", 
-                "min": 0, 
-                "max": 100, 
-                "step": 10, 
-                "suffix": "%",
-                "get_value": lambda: proxy.get_config("server","delay"), 
-                "set_value": lambda x: proxy.set_config("server", "delay", x)
-            },
-            {
-                "name": "Server delay time", 
+                "name": "Client delay time",
                 "min": 0,
-                "step": 100, 
+                "step": 100,
                 "suffix": "ms",
-                "get_value": lambda: proxy.get_config("server","delay_time"), 
-                "set_value": lambda x: proxy.set_config("server", "delay_time", x)
+                "get_value": lambda: proxy.get_config("client", "delay_time"),
+                "set_value": lambda x: proxy.set_config("client", "delay_time", x),
             },
-        ], 
-        10
+            {
+                "name": "Server drop",
+                "min": 0,
+                "max": 100,
+                "step": 10,
+                "suffix": "%",
+                "get_value": lambda: proxy.get_config("server", "drop"),
+                "set_value": lambda x: proxy.set_config("server", "drop", x),
+            },
+            {
+                "name": "Server delay",
+                "min": 0,
+                "max": 100,
+                "step": 10,
+                "suffix": "%",
+                "get_value": lambda: proxy.get_config("server", "delay"),
+                "set_value": lambda x: proxy.set_config("server", "delay", x),
+            },
+            {
+                "name": "Server delay time",
+                "min": 0,
+                "step": 100,
+                "suffix": "ms",
+                "get_value": lambda: proxy.get_config("server", "delay_time"),
+                "set_value": lambda x: proxy.set_config("server", "delay_time", x),
+            },
+        ],
+        10,
+    )
+
+    # Register signal handler
+    signal.signal(
+        signal.SIGINT, lambda sig, frame: signal_handler(sig, frame, proxy, cli)
     )
 
     with ThreadPoolExecutor() as executor:
@@ -142,4 +183,6 @@ if __name__ == "__main__":
     executor.shutdown(wait=True)
 
 
-
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
--- utils/cli.py
+++ utils/cli.py
@@ -1,25 +1,26 @@
-import time
 import curses
+import time
 from typing import List
 
-class CLI():
-    def __init__(self, titles: List[str], values, step = 10):
+
+class CLI:
+    def __init__(self, titles: List[str], values, step=10):
         self.titles = titles
         self.values = values
         self.step = step
-
+        self.running = False
 
     def display(self, stdscr):
         curses.curs_set(0)
         stdscr.nodelay(1)
-        selection = 0 
+        selection = 0
 
         stdscr.clear()
         for i in range(len(self.titles)):
             stdscr.addstr(i, 0, self.titles[i])
         stdscr.refresh()
 
-        while True:
+        while self.running:
             for i, item in enumerate(self.values):
                 if i == selection:
                     attr = curses.A_NORMAL
@@ -28,7 +29,12 @@ class CLI():
                     attr = curses.A_NORMAL
                     cursor = " "
 
-                value_str = "{cursor} {name}: {value}{suffix}\n".format(cursor=cursor, name=item["name"], value=item["get_value"](), suffix=item["suffix"])
+                value_str = "{cursor} {name}: {value}{suffix}\n".format(
+                    cursor=cursor,
+                    name=item["name"],
+                    value=item["get_value"](),
+                    suffix=item["suffix"],
+                )
                 stdscr.addstr(i + 3, 0, value_str, attr)
 
             key = stdscr.getch()
@@ -37,20 +43,28 @@ class CLI():
             elif key == curses.KEY_DOWN:
                 selection = (selection + 1) % len(self.values)
             elif key == curses.KEY_LEFT:
-                newValue = self.values[selection]['get_value']() - self.values[selection]['step']
+                newValue = (
+                    self.values[selection]["get_value"]()
+                    - self.values[selection]["step"]
+                )
                 if "min" in self.values[selection]:
-                    newValue = max(self.values[selection]['min'], newValue)
-                self.values[selection]['set_value'](newValue)
+                    newValue = max(self.values[selection]["min"], newValue)
+                self.values[selection]["set_value"](newValue)
             elif key == curses.KEY_RIGHT:
-                newValue = self.values[selection]['get_value']() + self.values[selection]['step']
+                newValue = (
+                    self.values[selection]["get_value"]()
+                    + self.values[selection]["step"]
+                )
                 if "max" in self.values[selection]:
-                    newValue = min(self.values[selection]['max'], newValue)
-                self.values[selection]['set_value'](newValue)
-        
+                    newValue = min(self.values[selection]["max"], newValue)
+                self.values[selection]["set_value"](newValue)
+
             stdscr.refresh()
             time.sleep(0.01)
 
     def start(self):
+        self.running = True
         curses.wrapper(self.display)
 
-
+    def stop(self):
+        self.running = False
