565d609cbb8ad8a07859731e30e5bdb8e5cc43f5
Reo Tamai <reotamai5@gmail.com>
2024-11-20 22:10:46 -0800

    removed unnecessary flag

--- utils/packet.py
+++ utils/packet.py
@@ -6,8 +6,7 @@ custom_header = {
     "syn": 1/8,
     "ack": 1/8,
     "fin": 1/8,
-    "rst": 1/8,
-    "offset": 4/8, # offset to make header length nice number
+    "offset": 5/8, # offset to make header length nice number
 }
 
 class Packet():
