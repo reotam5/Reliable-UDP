5ace065f36b9f2e2c5aac656575d7c67f2918aef
Reo Tamai <reotamai5@gmail.com>
2024-11-28 18:38:09 -0800

    simplified timeouts

--- utils/reliableUDP.py
+++ utils/reliableUDP.py
@@ -4,22 +4,19 @@ from typing import Any
 from utils.packet import Packet 
 from utils.fsm import FSM
 from random import randint
-from datetime import datetime, timedelta
 
 class ReliableUDP():
     BUFFER_SIZE = 1024
-    INITIAL_RETRANSMITTION_TIMER = 0.25
+    RETRIES = 20
 
-    def __init__(self, timeout=5):
+    def __init__(self, timeout=1):
         self.socket: socket
-        self.retransmit_timer = ReliableUDP.INITIAL_RETRANSMITTION_TIMER
-        self.timeout_disconnect = timeout
-        self.allowed_retries = self.timeout_disconnect // self.retransmit_timer
         self.message_pointer = 0 
         self.random_number = 0
         self.prev_random_number = None
         self.payload_size = 1
         self.target_addr: Any = None
+        self.retransmission_timeout = timeout
 
     def create(self):
         self.socket = socket(AF_INET, SOCK_DGRAM)
@@ -42,12 +39,17 @@ class ReliableUDP():
         self.flush_recv_buffer()
         self.message_pointer = 0 
         self.random_number = randint(1, 5000)
-        self.retransmit_timer = ReliableUDP.INITIAL_RETRANSMITTION_TIMER
 
-        def send_data(start_time = datetime.now()):
+        def send_data(retries = ReliableUDP.RETRIES):
             message_end_pointer = min(self.message_pointer + self.payload_size, len(message))
             is_first = self.message_pointer == 0
             is_last = message_end_pointer == len(message)
+
+            if retries < 1:
+                if not is_last:
+                    print(f"\033[91mMax retires reached({ReliableUDP.RETRIES}) {f"'{message[:self.message_pointer]}'" if self.message_pointer > 0 else ""} \033[0m")
+                return FSM.STATE.EXIT
+
             packet = Packet()
             packet.set_header_field("seq_num", str(self.message_pointer + self.random_number), base=10)
             packet.set_header_field("ack_num", "0", base=10)
@@ -57,15 +59,11 @@ class ReliableUDP():
                 packet.set_header_field("fin", "1", base=2)
             packet.set_payload(message[self.message_pointer:message_end_pointer])
             self.socket.sendto(packet.to_byte(), (str(ip), port))
-            return ("WAIT_ACK", is_last, message_end_pointer - self.message_pointer, start_time)
+            return ("WAIT_ACK", is_last, message_end_pointer - self.message_pointer, retries - 1)
 
-        def wait_ack(is_last, payload_length, start_time):
-            if datetime.now() - start_time > timedelta(seconds=self.timeout_disconnect):
-                if not is_last:
-                    print(f"\033[91mTIMEOUT: {(datetime.now() - start_time).total_seconds()} {f"'{message[:self.message_pointer]}'" if self.message_pointer > 0 else ""} \033[0m")
-                return FSM.STATE.EXIT
+        def wait_ack(is_last, payload_length, retries):
             try:
-                self.socket.settimeout(self.retransmit_timer)
+                self.socket.settimeout(self.retransmission_timeout)
                 data, _ = self.socket.recvfrom(1024)
                 packet = Packet(data)
                 ack_num = int(packet.get_header_field("ack_num", base=10))
@@ -75,18 +73,16 @@ class ReliableUDP():
                 is_fin = is_valid and packet.get_header_field("fin", base=2) == "1"
 
                 if not is_valid:
-                    return ("WAIT_ACK", is_last, payload_length, start_time)
+                    return ("WAIT_ACK", is_last, payload_length, retries)
                 if is_ack:
-                    self.retransmit_timer = min(max(self.retransmit_timer, (datetime.now() - start_time).total_seconds()), self.timeout_disconnect)
                     self.message_pointer = ack_num - self.random_number
                 if is_fin:
                     return ("SEND_ACK", seq_num)
                 if is_last:
-                    return ("WAIT_ACK", True, 0, start_time)
-                return ("SEND_DATA", datetime.now() if is_valid else start_time)
+                    return ("WAIT_ACK", True, 0, retries)
+                return ("SEND_DATA", ReliableUDP.RETRIES if is_valid else retries)
             except TimeoutError:
-                self.retransmit_timer = min(self.timeout_disconnect, self.retransmit_timer * 1.5)
-                return ("SEND_DATA", start_time)
+                return ("SEND_DATA", retries)
 
         def send_ack(last_seq_num):
             packet = Packet()
@@ -101,7 +97,7 @@ class ReliableUDP():
             [
                 { "source": FSM.STATE.START, "dest": "SEND_DATA", "action": send_data },
                 { "source": "SEND_DATA", "dest": "WAIT_ACK", "action": wait_ack },
-                { "source": "WAIT_ACK", "dest": FSM.STATE.EXIT, "action": self.flush_recv_buffer },
+                { "source": "SEND_DATA", "dest": FSM.STATE.EXIT, "action": self.flush_recv_buffer },
                 { "source": "WAIT_ACK", "dest": "SEND_DATA", "action": send_data },
                 { "source": "WAIT_ACK", "dest": "SEND_ACK", "action": send_ack },
                 { "source": "WAIT_ACK", "dest": "WAIT_ACK", "action": wait_ack },
@@ -150,11 +146,11 @@ class ReliableUDP():
             self.socket.sendto(packet.to_byte(), self.target_addr)
             
             if is_last_message:
-                return ("SEND_FIN", acknowledged_message, datetime.now())
+                return ("SEND_FIN", acknowledged_message, 20)
             return ("RECEIVE_DATA", acknowledged_message)
 
-        def send_fin(message, start_time):
-            if (datetime.now() - start_time) > timedelta(seconds=self.timeout_disconnect):
+        def send_fin(message, retries):
+            if retries < 1:
                 return (FSM.STATE.EXIT, message)
 
             packet = Packet()
@@ -163,11 +159,11 @@ class ReliableUDP():
             packet.set_header_field("seq_num", "0", base=10)
             packet.set_header_field("ack_num", str(self.random_number + len(message)), base=10)
             self.socket.sendto(packet.to_byte(), self.target_addr)
-            return ("WAIT_ACK", message, start_time)
+            return ("WAIT_ACK", message, retries - 1)
 
-        def wait_ack(message, start_time):
+        def wait_ack(message, retries):
             try:
-                self.socket.settimeout(ReliableUDP.INITIAL_RETRANSMITTION_TIMER)
+                self.socket.settimeout(0.5)
                 data, _ = self.socket.recvfrom(1024)
                 packet = Packet(data)
                 is_ack = packet.get_header_field("ack", base=2) == "1"
@@ -175,9 +171,9 @@ class ReliableUDP():
                 is_valid = ack_num  == 1
                 if is_ack and is_valid:
                     return (FSM.STATE.EXIT, message)
-                return ("SEND_FIN", message, start_time)
+                return ("SEND_FIN", message, retries)
             except TimeoutError:
-                return ("SEND_FIN", message, start_time)
+                return ("SEND_FIN", message, retries)
 
         def clean_up(message):
             self.message_pointer = 0 
